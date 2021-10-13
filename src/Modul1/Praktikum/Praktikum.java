488a6486ea739b319e54d9ad7c0b3731753b01dc
Raynaldi Zulfikar <20104042@ittelkom-pwt.ac.id>
2021-10-13 08:33:28 +0700

    Add Praktikum and Tugas File

---
+++ src/Modul1/Praktikum/Praktikum.java
@@ -0,0 +1,7 @@
+package Modul1.Praktikum;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/Modul1/Tugas/Tugas.java
@@ -0,0 +1,7 @@
+package Modul1.Tugas;
+
+public class Tugas {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
