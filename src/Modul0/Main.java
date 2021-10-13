31119af0843a4705af166ce56c0de9ad5ca23632
Raynaldi Zulfikar <20104042@ittelkom-pwt.ac.id>
2021-10-13 08:16:41 +0700

    Add Looping Statement

--- src/Modul0/Main.java
+++ src/Modul0/Main.java
@@ -3,5 +3,8 @@ package Modul0;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("*");
+        }
     }
 }
