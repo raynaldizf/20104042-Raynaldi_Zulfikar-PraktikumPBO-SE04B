467cf9f69863f9166b16712659a2dff1d2e97c42
Raynaldi Zulfikar <20104042@ittelkom-pwt.ac.id>
2021-10-23 11:04:05 +0700

    Add Tugas Praktikum

---
+++ src/Modul2/Tugas/Tugas1.java
@@ -0,0 +1,10 @@
+package Modul2.Tugas;
+
+public class Tugas1 {
+    public static void main(String[] args) {
+        // Untuk menulis integer 2147483648 kita harus menambahkan L atau l dibelakangnya
+        // karena nilai 2147483648 melewati batas range dari nilai int.
+        long p=2147483648L;
+        System.out.println(p);
+    }
+}
