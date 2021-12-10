c3eea707c2bb17a565e8d51020f772bb02169f2c
Raynaldi Zulfikar <51937537+raynaldizf@users.noreply.github.com>
2021-12-10 21:52:45 +0700

    Praktikum Modul 7

--- src/Tugas/Percobaan2/Overload1.java
+++ src/Tugas/Percobaan2/Overload1.java
@@ -2,15 +2,15 @@ package Tugas.Percobaan2;
 
 public class Overload1 {
     void myMethod(short s) {
-        System.out.println("short");
+        System.out.println("Short");
     }
 
     void myMethod(int i) {
-        System.out.println("int");
+        System.out.println("Int");
     }
 
     void myMethod(long l) {
-        System.out.println("long");
+        System.out.println("Long");
     }
 
     public static void main(String[] args) {
